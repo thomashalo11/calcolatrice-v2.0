96db84b90fe41abfda42377e4fa8a093e942fdf0
thomashalo11 <117899427+thomashalo11@users.noreply.github.com>
2024-01-19 10:58:48 +0100

    Add files via upload

---
+++ server.py
@@ -0,0 +1,57 @@
+import socket
+import json
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 5005
+BUFFER_SIZE = 1024
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((SERVER_IP, SERVER_PORT))
+sock.listen(5)
+
+msg = ""
+
+while True:
+    client, address = sock.accept()
+    print(f"Connessione stabilita: - {address[0]}:{address[1]}")
+    
+    """
+    data, addr = sock.recvfrom(BUFFER_SIZE)
+    msg = data.decode()
+    msg = json.loads(data)
+    """
+    
+    # Se l' input è "Esci", viene conclusa la connessione
+    if msg == "Esci":
+        print("Connessione conclusa")
+        break
+    
+    risultato = 0
+    
+    print("Lista: ", msg)
+    operando1 = msg["primoNumero"]
+    operazione = msg["operazione"]
+    operando2 = msg["secondoNumero"]
+    
+    print(operando1)
+    print(operazione)
+    print(operando2)
+    
+    # Trasformiamo le stringhe in int
+    num1 = int(operando1)
+    num2 = int(operando2)
+    
+    if operazione == "+":
+        risultato = num1 + num2
+    elif operazione == "-":
+        risultato = num1 - num2
+    elif operazione == "/":
+        risultato = num1 / num2
+    elif operazione == "*":
+        risultato = num1 * num2
+    
+    output = str(risultato)
+    output = json.dumps(output)
+    output = output.encode()
+    
+    sock.send(output)
